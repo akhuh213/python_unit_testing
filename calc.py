a1a271606b731b4de5c02f9bd20d9e9f24829707
Alice Huh <aklee213@gmail.com>
2020-09-24 15:24:24 -0400

    [test] add test

---
+++ calc.py
@@ -0,0 +1,22 @@
+def add(x, y):
+    """Add Function"""
+
+    return x + y
+
+def subtract(x, y):
+    """Subtract Function"""
+
+    return x - y
+
+
+def multiply(x, y):
+    """Add Function"""
+
+    return x * y
+
+def divide(x, y):
+    """Add Function"""
+
+    return x / y
+
+print(add(10,10))
